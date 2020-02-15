a2e229f3d3b36298820953a4cdbcb8e844a35fe4
exark <47224170+exark@users.noreply.github.com>
2020-02-15 17:53:20 +0100

    modification entite formulaire, et suppression des classes demande, attestation et reclamation

--- SchoolGest/src/Entite/Formulaire/Formulaire.java
+++ SchoolGest/src/Entite/Formulaire/Formulaire.java
@@ -15,63 +15,72 @@ public class Formulaire {
     private int idFormulaire;
     private String objet;
     private String description;
+    private String fichier;
     private boolean validation;
     private Date dateEnvoi;
+    
 
     public Formulaire() {
     }
 
-    
-    public Formulaire( String description, String objet) {
-        this.description = description;
+    public Formulaire(String objet, String description, String fichier, boolean validation) {
         this.objet = objet;
+        this.description = description;
+        this.fichier = fichier;
+        this.validation = validation;
     }
 
-    public Formulaire(int idFormulaire, String objet, String description, boolean validation, Date dateEnvoi) {
+    public Formulaire(int idFormulaire, String objet, String description, String fichier, boolean validation, Date dateEnvoi) {
         this.idFormulaire = idFormulaire;
         this.objet = objet;
         this.description = description;
+        this.fichier = fichier;
         this.validation = validation;
         this.dateEnvoi = dateEnvoi;
     }
-    
 
     public int getIdFormulaire() {
         return idFormulaire;
-    }
-
-    public String getDescription() {
-        return description;
     }
 
     public String getObjet() {
         return objet;
     }
 
-    public Date getDateEnvoi() {
-        return dateEnvoi;
+    public String getDescription() {
+        return description;
+    }
+
+    public String getFichier() {
+        return fichier;
     }
 
     public boolean isValidation() {
         return validation;
     }
 
-    public void setValidation(boolean validation) {
-        this.validation = validation;
+    public Date getDateEnvoi() {
+        return dateEnvoi;
     }
-    
-    
 
     public void setIdFormulaire(int idFormulaire) {
         this.idFormulaire = idFormulaire;
+    }
+
+    public void setObjet(String objet) {
+        this.objet = objet;
     }
 
     public void setDescription(String description) {
         this.description = description;
     }
 
-    public void setObjet(String objet) {
-        this.objet = objet;
+    public void setFichier(String fichier) {
+        this.fichier = fichier;
+    }
+
+    public void setValidation(boolean validation) {
+        this.validation = validation;
     }
 
     public void setDateEnvoi(Date dateEnvoi) {
@@ -80,8 +89,12 @@ public class Formulaire {
 
     @Override
     public String toString() {
-        return "Formulaire{" + "idFormulaire=" + idFormulaire + ", objet=" + objet + ", description=" + description + ", validation=" + validation + ", dateEnvoi=" + dateEnvoi + '}';
+        return "Formulaire{" + "idFormulaire=" + idFormulaire + ", objet=" + objet + ", description=" + description + ", fichier=" + fichier + ", validation=" + validation + ", dateEnvoi=" + dateEnvoi + '}';
     }
+
+    
+
+    
 
     
     
